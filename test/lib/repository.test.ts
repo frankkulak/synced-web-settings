d306ac5e9cc23a84060b5e0510beb76641114021
frankkulak <fs.kulak@gmail.com>
2024-04-05 22:03:55 -0400

    Add stubs for repo tests.

--- test/lib/repository.test.ts
+++ test/lib/repository.test.ts
@@ -7,48 +7,124 @@ import MockStorage from "../mocks/mock-storage";
 describe("SettingsRespository", () => {
   const prefix = "setting:";
   const storage = new MockStorage();
+  const callbackValues = new Map<keyof MockSettings, any[]>();
   let repo: SettingsRespository<MockSettings>;
+
+  function onCallback<T extends keyof MockSettings>(setting: T, value: MockSettings[T]) {
+    const array = callbackValues.get(setting) ?? [];
+    array.push(value);
+    callbackValues.set(setting, array);
+  }
 
   beforeEach(() => {
     storage.clear();
+    callbackValues.clear();
     repo = new SettingsRespository<MockSettings>({
       prefix: prefix,
       storage: storage,
       settings: {
         boolean: {
           type: types.StoredBoolean,
-          defaultValue: false
+          defaultValue: false,
+          callbacks: [
+            (value) => onCallback("boolean", value)
+          ]
         },
         number: {
           type: types.StoredNumber,
-          defaultValue: 0
+          defaultValue: 0,
+          callbacks: [
+            (value) => onCallback("number", value)
+          ]
         },
         string: {
           type: types.StoredString,
-          defaultValue: ""
+          defaultValue: "",
+          callbacks: [
+            (value) => onCallback("string", value)
+          ]
         },
         json: {
           type: types.StoredJson,
-          defaultValue: { numbers: [] }
+          defaultValue: { numbers: [] },
+          callbacks: [
+            (value) => onCallback("json", value)
+          ]
         }
       },
     });
   });
 
-  describe("#constructor()", () => {
-    it("", () => {
-      // TODO:
-    });
-  });
-
   describe("#settings", () => {
-    it("", () => {
-      // TODO:
+    describe("#get()", () => {
+      it("should use the correct prefix when reading from storage", () => {
+        // TODO:
+      });
+
+      it("should return default values for settings that don't exist", () => {
+        // TODO:
+      });
+
+      it("should return correct values for settings that do exist", () => {
+        // TODO:
+      });
+
+      it("should fetch value from storage even if set() was not used", () => {
+        // TODO:
+      });
+    });
+
+    describe("#set()", () => {
+      it("should use the correct prefix when writing to storage", () => {
+        // TODO:
+      });
+
+      it("should update value in storage", () => {
+        // TODO:
+      });
+
+      it("should update value when fetched with get()", () => {
+        // TODO:
+      });
+
+      it("should trigger setting's callback functions", () => {
+        // TODO:
+      });
+
+      it("should notify all subscribers", () => {
+        // TODO:
+      });
+    });
+
+    describe("#deleteProperty()", () => {
+      it("should use the correct prefix when deleting from storage", () => {
+        // TODO:
+      });
+
+      it("should remove setting from storage", () => {
+        // TODO:
+      });
+
+      it("should update value when fetched with get() to default", () => {
+        // TODO:
+      });
+
+      it("should trigger setting's callback functions", () => {
+        // TODO:
+      });
+
+      it("should notify all subscribers", () => {
+        // TODO:
+      });
     });
   });
 
   describe("#subscriptions", () => {
-    it("", () => {
+    it("should notify subscribers when a change is made to settings", () => {
+      // TODO:
+    });
+
+    it("should stop notifying subscribers when unsub is called", () => {
       // TODO:
     });
   });
